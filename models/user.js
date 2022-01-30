2463aa757dbccf1634cf0844de37d802ca72d428
kobyrthr-dev <kobyrthr@gmail.com>
2022-01-30 13:17:07 -0500

    set user schema id to googleId to fix the serialized user bug

--- models/user.js
+++ models/user.js
@@ -2,15 +2,15 @@ const mongoose = require('mongoose');
 const Product = require('../models/products')
 
 const userSchema = new mongoose.Schema({
-    id:{type: String},
+    // id:{type: String},
+    googleId: String ,
     name: String,
     email: String,
     Location: String, //could use google auth var
     products: [{
         type: mongoose.Schema.Types.ObjectId,
     ref: 'Product'
-}],
-    googleId: String ,
+}]
 },
 {
     timestamps:true
