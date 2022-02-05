66bec0f66af12af1f1897ed45c0fc4c0efb3dec4
kobyrthr-dev <kobyrthr@gmail.com>
2022-02-05 18:07:02 -0500

    got the product id to submit to the user product array

--- controllers/index.js
+++ controllers/index.js
@@ -69,9 +69,9 @@ function create(req,res){
     User.findById(newProd.seller).exec(function (err, foundUser) {
         if (err) res.send(err);
         console.log('this is the found user',foundUser)
-        console.log('this is newProd',newProd._id)
-        // foundUser.products.push(newProd); 
-        // foundUser.save(); 
+        console.log('this is newProdId',newProd._id)
+        foundUser.products.push(newProd._id); 
+        foundUser.save(); 
     });
     res.redirect('/')
 }
