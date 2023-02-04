a40d39df85a0dd77f8112d0c431357a8d0aff4ec
Karina Garces ✨ <karinadgarces@gmail.com>
2023-02-04 15:59:20 -0500

    allow s3 assets module to locate webapp

--- cdk-savvy/lib/cdk-savvy-stack.js
+++ cdk-savvy/lib/cdk-savvy-stack.js
@@ -1,5 +1,4 @@
-import s3assets = require('@aws-cdk/aws-s3-assets');
-
+const s3assets = require('@aws-cdk/aws-s3-assets');
 const cdk = require('@aws-cdk/core');
 // const sqs = require('@aws-cdk/aws-sqs');
 
@@ -13,7 +12,23 @@ class CdkSavvyStack extends cdk.Stack {
   constructor(scope, id, props) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    
+
+    /**
+     *    The code that defines your stack goes here
+     * 
+     * 
+     * This code uses the S3 Assets module and 
+     * takes the zip file of the web app located in 
+     * the root of the CDK app, and uploads it to S3. 
+     * Whenever you update the zip file and you deploy 
+     * this stack, the file will get updated in S3.
+     */
+
+    
+const webAppZipArchive = new s3assets.Asset(this, 'WebAppZip', {
+  path: `${__dirname}/../app.zip`
+});
 
     // example resource
     // const queue = new sqs.Queue(this, 'CdkSavvyQueue', {
