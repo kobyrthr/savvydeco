260fa4fea90aeded910b644a29f1bf9390786ceb
Karina Garces ✨ <karinadgarces@gmail.com>
2023-02-05 11:39:19 -0500

    cdk deploy successful, pending env testing

--- cdk-savvy/lib/cdk-savvy-stack.js
+++ cdk-savvy/lib/cdk-savvy-stack.js
@@ -1,7 +1,7 @@
 const s3assets = require('@aws-cdk/aws-s3-assets');
-const iam = require('@aws-cdk/iam');
+const iam = require('@aws-cdk/aws-iam');
 const cdk = require('@aws-cdk/core');
-const elasticbeanstalk = require('@aws-cdk/easticbeanstalk');
+const elasticbeanstalk = require('@aws-cdk/aws-elasticbeanstalk');
 
 // const sqs = require('@aws-cdk/aws-sqs');
 
@@ -58,34 +58,49 @@ const instanceProfile = new iam.CfnInstanceProfile(this, myProfileName, {
   ]
 });
 
+
+// Example of some options which can be configured
 const optionSettingProperties = [
   {
       namespace: 'aws:autoscaling:launchconfiguration',
       optionName: 'IamInstanceProfile',
-      value: myProfileName
+      value: myProfileName,
   },
   {
       namespace: 'aws:autoscaling:asg',
       optionName: 'MinSize',
-      value: '1'
+      value: '1',
   },
   {
       namespace: 'aws:autoscaling:asg',
       optionName: 'MaxSize',
-      value: '1'
+      value: '1',
   },
   {
       namespace: 'aws:ec2:instances',
       optionName: 'InstanceTypes',
-      value: 't2.micro'
+      value: 't2.micro',
   },
 ];
+
+
+const appVersionProps = new elasticbeanstalk.CfnApplicationVersion(this, 'AppVersion', {
+  applicationName: appName,
+  sourceBundle: {
+      s3Bucket: webAppZipArchive.s3BucketName,
+      s3Key: webAppZipArchive.s3ObjectKey,
+  },
+});
+
+
+// check app exists before creating app version
+appVersionProps.addDependsOn(app);
 
 // Create an Elastic Beanstalk environment to run the application
 const elbEnv = new elasticbeanstalk.CfnEnvironment(this, 'Environment', {
   environmentName: 'SavvyApp',
   applicationName: app.applicationName || appName,
-  solutionStackName: '64bit Amazon Linux 2 v5.6.1 running Node.js 14',
+  solutionStackName: '64bit Amazon Linux 2 v5.6.4 running Node.js 14',
   optionSettings: optionSettingProperties,
   versionLabel: appVersionProps.ref,
 });
