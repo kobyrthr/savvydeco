2fa974f765e0369bff9e40d78f605fdea2310085
Karina Garces ✨ <karinadgarces@gmail.com>
2023-02-17 22:10:56 -0500

    fix for logout error callback error

--- routes/index.js
+++ routes/index.js
@@ -17,9 +17,12 @@ router.get('/oauth2callback', passport.authenticate("google",{
 );
 
 //logout router
-router.get("/logout", (req,res)=> {
-    req.logout();
-    res.redirect("/");
+router.get("/logout", (req,res, next)=> {
+    req.logout(function(err){
+        if(err){
+            return next(err);  }
+            res.redirect("/");
+    })
 })
 
 module.exports = router,
