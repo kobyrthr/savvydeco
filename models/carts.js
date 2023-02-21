62128420353c722c0e745cc0879a3943d00a990d
Karina Garces ✨ <karinadgarces@gmail.com>
2023-02-21 14:17:21 -0500

    cart item and shopping cart schemas

---
+++ models/carts.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const user = require('../models/user');
+const products = require('../models/products');
+
+
+
+const cartItemSchema = new Schema({
+  product: {
+    type: Schema.Types.ObjectId,
+    ref: 'products',
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+    min: 1,
+  },
+});
+
+
+const shoppingCartSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'user',
+    required: true,
+  },
+  items: [cartItemSchema],
+}, {
+  timestamps: true,
+});
+
+const ShoppingCart = mongoose.model('ShoppingCart', shoppingCartSchema);
+
+module.exports = ShoppingCart;
